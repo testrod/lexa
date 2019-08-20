98d77204c411d0e326b8b9d18b49fac901125680
testrod <33133548+testrod@users.noreply.github.com>
2019-08-20 12:38:41 -0300

    Update lexa.py

--- lexa.py
+++ lexa.py
@@ -3,13 +3,13 @@
 import sys	#Library to provide access to some variables used
 import xlrd #Library to extract data from Microsoft Excel
 
-inputfile = sys.argv[1]		#inputfile name
-outputfile = sys.argv[2]	#outputfile new_file
+inputfile = sys.argv[1] #inputfile name
+outputfile = sys.argv[2] #outputfile new_file
 num_lines = 0				
 total_num_lines = 0
 
-workbook = xlrd.open_workbook(inputfile, on_demand = True) 	#open the xls file to be procesed
-new_file = open(outputfile,"w")								#open a new txt file to save the results 
+workbook = xlrd.open_workbook(inputfile, on_demand = True)  #open the xls file to be procesed
+new_file = open(outputfile,"w")	 #open a new txt file to save the results 
 totalsheets = len(workbook.sheet_names())
 
 print "This xls file has", totalsheets, "sheets"
@@ -18,9 +18,9 @@ print "The input file ", inputfile ," is being procesed"
 i = 0
 while i < totalsheets:
 
-	sheet0 = workbook.sheet_by_index(i)						#set the first sheet to be procesed
-	c = 2													#start from the second row
-	rows = sheet0.nrows										#total of rows
+	sheet0 = workbook.sheet_by_index(i) #set the first sheet to be procesed
+	c = 2 #start from the second row
+	rows = sheet0.nrows #total of rows
 
 	while c < rows:
 		cl = sheet0.cell(c, 0).value
